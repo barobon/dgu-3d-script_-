f0a9e2fb4793c34466d5b43f83be42645dd8d19e
barobon <46195201+barobon@users.noreply.github.com>
2020-06-07 23:44:31 +0900

    캐릭터 이동 구현

---
+++ 3d스크립트_기말과제/Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    Animator animator;
+    Vector3 move;
+    Quaternion rotation = Quaternion.identity;
+    Rigidbody m_rigidbody;
+
+    public float turnSpeed = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        m_rigidbody.MovePosition(transform.position + move * speed * Time.deltaTime);
+        m_rigidbody.MoveRotation(rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        move.Set(horizontal, 0f, vertical);
+        move.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isRunning = hasHorizontalInput || hasVerticalInput;
+
+        animator.SetBool("isRunning", isRunning);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, move, turnSpeed * Time.deltaTime, 0f);
+        rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+
+
+}

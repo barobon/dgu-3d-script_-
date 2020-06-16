4a89d6b231d9b95e9c341d4988f9f28efa6d2aa1
barobon <46195201+barobon@users.noreply.github.com>
2020-06-16 21:07:09 +0900

    점프구현현

--- 3d스크립트_기말과제/Assets/Scripts/Movement.cs
+++ 3d스크립트_기말과제/Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed = 10f;
+    public float jumpForce = 6f;
 
     Animator animator;
     Vector3 move;
@@ -12,6 +13,7 @@ public class Movement : MonoBehaviour
     Rigidbody m_rigidbody;
 
     public float turnSpeed = 30f;
+    private bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +26,7 @@ public class Movement : MonoBehaviour
     {
         m_rigidbody.MovePosition(transform.position + move * speed * Time.deltaTime);
         m_rigidbody.MoveRotation(rotation);
+        Jump();
     }
 
     // Update is called once per frame
@@ -40,11 +43,44 @@ public class Movement : MonoBehaviour
         bool isRunning = hasHorizontalInput || hasVerticalInput;
 
         animator.SetBool("isRunning", isRunning);
-
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, move, turnSpeed * Time.deltaTime, 0f);
         rotation = Quaternion.LookRotation(desiredForward);
+        //rotation = Quaternion.Slerp(transform.rotation, cam.rotation, turnSpeed * Time.deltaTime);
+  
     }
 
 
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!isJumping)
+            {
+                isJumping = true;
+                animator.SetBool("isJumping", isJumping);
+                //animator.SetBool("isRunning", false);
+                m_rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+            else
+                return;
+        }
+            
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if (isJumping)
+            {
+                isJumping = false;
+                animator.SetBool("isJumping", isJumping);
+                m_rigidbody.velocity = Vector3.zero;
+            }
+            //animator.SetBool("isJumping", isJumping);
+        }
+    }
 
 }
+

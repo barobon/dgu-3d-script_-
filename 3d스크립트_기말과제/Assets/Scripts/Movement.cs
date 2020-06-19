65e831155e43839a6f55e2164488c946b105a20a
barobon <46195201+barobon@users.noreply.github.com>
2020-06-19 14:25:03 +0900

    어색한 이동, 점프 개선

--- 3d스크립트_기말과제/Assets/Scripts/Movement.cs
+++ 3d스크립트_기말과제/Assets/Scripts/Movement.cs
@@ -6,14 +6,18 @@ public class Movement : MonoBehaviour
 {
     public float speed = 10f;
     public float jumpForce = 6f;
+    public float turnSpeed = 30f;
 
     Animator animator;
     Vector3 move;
     Quaternion rotation = Quaternion.identity;
     Rigidbody m_rigidbody;
 
-    public float turnSpeed = 30f;
+    private float horizontal = 0;
+    private float vertical = 0;
+
     private bool isJumping = false;
+    private bool isGrounded = true;
 
     // Start is called before the first frame update
     void Start()
@@ -24,31 +28,40 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        m_rigidbody.MovePosition(transform.position + move * speed * Time.deltaTime);
-        m_rigidbody.MoveRotation(rotation);
+        Run();
+        Rotate();
         Jump();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        move.Set(horizontal, 0f, vertical);
-        move.Normalize();
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isRunning = hasHorizontalInput || hasVerticalInput;
 
+        if (isJumping && m_rigidbody.velocity.y < 0)
+        {
+            m_rigidbody.mass = 3;
+        }
+
         animator.SetBool("isRunning", isRunning);
-        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, move, turnSpeed * Time.deltaTime, 0f);
-        rotation = Quaternion.LookRotation(desiredForward);
-        //rotation = Quaternion.Slerp(transform.rotation, cam.rotation, turnSpeed * Time.deltaTime);
-  
     }
 
+    void Run()
+    {
+        move = vertical * transform.forward * speed * Time.deltaTime;
+        m_rigidbody.MovePosition(m_rigidbody.position + move);
+    }
+
+    void Rotate()
+    {
+        float rotation = horizontal * turnSpeed * Time.deltaTime;
+        m_rigidbody.rotation *= Quaternion.Euler(0, rotation, 0);
+    }
 
     void Jump()
     {
@@ -57,11 +70,15 @@ public class Movement : MonoBehaviour
             if (!isJumping)
             {
                 isJumping = true;
+                isGrounded = false;
+
                 animator.SetBool("isJumping", isJumping);
-                //animator.SetBool("isRunning", false);
+                animator.SetBool("isGrounded", isGrounded);
+
+                m_rigidbody.velocity = Vector3.zero;
                 m_rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             }
-            else
+            else 
                 return;
         }
             
@@ -75,7 +92,12 @@ public class Movement : MonoBehaviour
             if (isJumping)
             {
                 isJumping = false;
+                isGrounded = true;
+
                 animator.SetBool("isJumping", isJumping);
+                animator.SetBool("isGrounded", isGrounded);
+
+                m_rigidbody.mass = 1;
                 m_rigidbody.velocity = Vector3.zero;
             }
             //animator.SetBool("isJumping", isJumping);
